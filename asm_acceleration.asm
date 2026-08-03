0abdcf7f1a13a54bf7e9133a9ce14ee905e121f6
Ramuel Cordero <virkato71@gmail.com>
2026-08-03 19:17:16 +0800

    Update asm_acceleration.asm

--- asm_acceleration.asm
+++ asm_acceleration.asm
@@ -1,14 +1,27 @@
+section .data
+    align 8
+    c1000: dq 1000.0
+    c3600: dq 3600.0
+
 section .text
 
 default rel
-bits 64
+bits64
 
 global asm_acceleration
 asm_acceleration:
+    movsd   xmm3, [c1000]
+    movsd   xmm4, [c3600]
 
-	subsd   xmm1, xmm0        
-    divsd   xmm1, xmm2         
+    mulsd   xmm0, xmm3
+    divsd   xmm0, xmm4
 
-    cvttsd2si eax, xmm1
-	
-	ret
+    mulsd   xmm1, xmm3
+    divsd   xmm1, xmm4
+
+    subsd   xmm1, xmm0
+    divsd   xmm1, xmm2
+
+    cvtsd2si eax, xmm1
+
+    ret
