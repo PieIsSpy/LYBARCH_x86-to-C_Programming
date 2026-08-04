d46917863003e227f7050126dee5698bf3c981d5
PieIsSpy <karldeejayomandac@gmail.com>
2026-08-04 11:14:49 +0800

    docs: added comments for important sections

--- asm_acceleration.asm
+++ asm_acceleration.asm
@@ -13,15 +13,19 @@ asm_acceleration:
     movsd   xmm3, [c1000]
     movsd   xmm4, [c3600]
 
+    ; convert vi to m/s
     mulsd   xmm0, xmm3
     divsd   xmm0, xmm4
 
+    ; convert vf to m/s
     mulsd   xmm1, xmm3
     divsd   xmm1, xmm4
 
+    ; calculate acceleration
     subsd   xmm1, xmm0
     divsd   xmm1, xmm2
 
+    ; convert to int
     cvtsd2si eax, xmm1
 
     ret
